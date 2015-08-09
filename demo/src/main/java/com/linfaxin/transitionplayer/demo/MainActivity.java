67710358cbc1597809b0f4a936a8261ea7a87b8d
linfaxin <linfaxin@xiaomashijia.com>
2015-08-10 00:23:50 +0800

    [remove] remove import

--- demo/src/main/java/com/linfaxin/transitionplayer/demo/MainActivity.java
+++ demo/src/main/java/com/linfaxin/transitionplayer/demo/MainActivity.java
@@ -3,8 +3,6 @@ package com.linfaxin.transitionplayer.demo;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 
 
